53b5d48b40498c661608939568bae5b61a4ccd0a
Patient Developer <pantient.developer@posteo.de>
2021-03-24 21:29:36 +0100

    SD-2 prepare user authentication
    Add User model and password check with SqlAlchemy and werkzeug.security
    package.

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,19 @@
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
+from flask_login import login_user
 
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', form=form)
