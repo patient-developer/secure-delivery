a6f40032669f2e366553cb567ee164bcfae33047
patient-developer <81027176+patient-developer@users.noreply.github.com>
2021-04-15 22:31:54 +0200

    SD-4 pdf encryption and password e-mail (#4)
    Co-authored-by: Patient Developer <pantient.developer@posteo.de>

--- app/emails.py
+++ app/emails.py
@@ -5,5 +5,8 @@ from app import mail
 def send_mail(subject, sender, recipients, text_body, attachment_name, attachment):
     message = Message(subject=subject, sender=sender, recipients=recipients)
     message.body = text_body
-    message.attach(attachment_name, 'application/octect-stream', attachment)
+
+    if attachment_name is not None:
+        message.attach(attachment_name, 'application/octect-stream', attachment)
+
     mail.send(message)
